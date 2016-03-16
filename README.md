349e66e5f3f7abdcf6bcbf7efda85bad946a8ea7
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2016-03-16 13:50:41 +0100

    Changed emgu-3.0 to emgu-3.1 added lena as hello world

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Kod projektu: AS15
 
 Data set:
 http://www5.cs.fau.de/research/data/fundus-images
+
+Tutorials for emguCV:
+http://www.emgu.com/wiki/index.php/Tutorial
+
+Download and instalation tutorial (shouldn't be needed added just in case):
+http://www.emgu.com/wiki/index.php/Download_And_Installation#Windows
--- RecognitionOfCapillaryNetworks/RecognitionOfCapillaryNetworks/Program.cs
+++ RecognitionOfCapillaryNetworks/RecognitionOfCapillaryNetworks/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace RecognitionOfCapillaryNetworks
 {
@@ -16,7 +19,11 @@ namespace RecognitionOfCapillaryNetworks
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
+            Mat img =  CvInvoke.Imread("lena.jpg", Emgu.CV.CvEnum.LoadImageType.AnyColor);
+            CvInvoke.Imshow("Window", img);
+            CvInvoke.WaitKey(0);  //Wait for the key pressing event
         }
     }
 }
